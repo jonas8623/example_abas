f17107ab110ceb946368805b93a69f985ec0aeab
Jonas Felipe Quirino <jonas-felipe16@hotmail.com>
2023-09-13 12:12:18 -0300

    add styles

--- app/src/main/java/com/example/abas/MainActivity.java
+++ app/src/main/java/com/example/abas/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.abas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.abas.fragments.AboutFragment;
+import com.example.abas.fragments.ContactFragment;
+import com.example.abas.fragments.HomeFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        // Configurar adapter para abas
+        FragmentPagerItemAdapter fragmentPagerItemAdapter = new FragmentPagerItemAdapter(getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Home", HomeFragment.class)
+                .add("Contact", ContactFragment.class)
+                .add("About", AboutFragment.class)
+                .create());
+
+        viewPager.setAdapter(fragmentPagerItemAdapter);
+        smartTabLayout.setViewPager(viewPager);
     }
 }
